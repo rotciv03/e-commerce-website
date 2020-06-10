e4b66960b288ea0f6bb56f0ddd3c4fb3c65aee28
admin <admin@admins-MBP.lan>
2020-06-09 20:03:00 -0700

    broke homepage into smaller components, and finished stying our menu item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -45,7 +45,7 @@ class Directory extends React.Component {
     };
    }
 
-render(){
+ render(){
        return(
         <div className='directory-menu'>
            {this.state.sections.map(({title, imageUrl, id, size}) => (
