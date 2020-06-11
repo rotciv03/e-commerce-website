be2d78530e5bcbb9da5d57c2b4bc86443d828aca
rotciv03 <vctrsls3@gmail.com>
2020-06-11 12:10:56 -0700

    updating our app with a shop page, collection preview component and collection item component

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import CollectionItem from "../collection-item/collection-item.component";
+import "./collection-preview.styles.scss";
+import "./collection-preview.styles.scss";
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
+    </div>
+  </div>
+);
+//everytime is component gets render so the anonymous funtions calls gett called and rerendered so it does raise a cocern of perfomane
+export default CollectionPreview;
